21d7ecf5b9af41b8ab1ac57c4ffce6f626943ea3
sparkz <sparkz@pop-os.localdomain>
2021-06-10 17:46:32 +0300

    Install dependencies if missing

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+if ! test -x /usr/bin/qmake
+then
+    echo "Installing missing qt5 packages..."
+    sudo apt-get install -y qt5-default >> /dev/null
+fi
+
 if ! [ -z $1 ]; then
     if [ $1 = 'rm' ]; then
-        rm sudoku
+        rm -r game
         echo "Executable removed."
         exit 0
     fi
@@ -10,12 +17,18 @@ fi
 mkdir -p game
 mkdir -p game/saves
 
+echo "Compiling project..."
+
 cd impl
-cmake .
-make 
+cmake . 
+make --no-print-directory
+
+echo "Removing makefiles..."
 
 rm -rf Makefile CMakeCache.txt  CMakeFiles/ cmake_install.cmake sudoku_autogen/ *.cpp
 
 mv sudoku ../game
+
+echo "Done!"
 
 exit 0;
